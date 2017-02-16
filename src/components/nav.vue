be96651f3c2a4a2ca57d99373139751a48a2be66
Cong.Zhang <cong.zhang@daheng.co>
2017-02-16 17:43:58 +0800

    add

---
+++ src/components/nav.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>content</h1>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'nav',
+    data() {
+      return {
+      }
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,55 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from 'components/Hello'
-
+import main from 'components/main'
+import login from 'components/login'
 Vue.use(Router)
 
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'main',
+//       component: main
+//     },
+//     {
+//       path: '/login',
+//       name: 'login',
+//       component: require('components/login')
+//     },
+//   ]
+// })
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path:'/',
+      redirect: login
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: require('components/main'),
+      children: [
+        {
+          path: '/web1',
+          name: 'web1',
+          component: require('components/web1')
+        },
+        {
+          path: '/web2',
+          name: 'web2',
+          component: require('components/web2')
+        },
+        {
+          path: '/web3',
+          name: 'web3',
+          component: require('components/web3')
+        } 
+      ]
     }
   ]
-})
+});
